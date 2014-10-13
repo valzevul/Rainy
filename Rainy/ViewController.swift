381cdac1c3a475e8432e3f632e22d3e203543081
Vadim Drobinin <vadim@drobinin.com>
2014-10-13 13:24:24 +0400

    Initial commit from local repo

---
+++ Rainy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rainy
+//
+//  Created by Vadim Drobinin on 13/10/14.
+//  Copyright (c) 2014 Vadim Drobinin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RainyTests/RainyTests.swift
@@ -0,0 +1,36 @@
+//
+//  RainyTests.swift
+//  RainyTests
+//
+//  Created by Vadim Drobinin on 13/10/14.
+//  Copyright (c) 2014 Vadim Drobinin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RainyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
