9968fe4130d64add09c534cfd9eac8a5b71087ed
Vadim Drobinin <vadim@drobinin.com>
2014-10-13 13:30:12 +0400

    Status bar is disabled.

--- Rainy/AppDelegate.swift
+++ Rainy/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Because at the screen time of the last update will be provided
+        application.setStatusBarHidden(true, withAnimation: .None)
+        
         // Override point for customization after application launch.
         return true
     }
